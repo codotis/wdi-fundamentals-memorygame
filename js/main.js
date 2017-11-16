cc612004f314fc513658ec364bcec91261a0def9
Otis Vickers-Graver <otis@Otiss-MacBook-Air.local>
2017-11-16 19:38:45 +0000

    created memory game

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 console.log("Up and running!");
 
-
-/*console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)*/
-
 var cards = [
 {
 	rank: "queen",
@@ -27,6 +23,7 @@ var cards = [
 },
 ];
 
+
 var cardsInPlay = [];
 
 
@@ -36,29 +33,46 @@ var checkForMatch = function() {
 	} else {
 		alert("Sorry, try again.");
 	}	
-}
+};
 
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-
-
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit)
+	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 
 
-
+if (cardsInPlay.length === 2); {
 checkForMatch();
+	}
+
+};
 
 
-/*if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}*/
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i ++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard);
+	document.getElementById('game-board').appendChild(cardElement);
+	}
 
-}
-flipCard(0);
-flipCard(2);
+};
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
